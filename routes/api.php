ca85032e76862d8ba3444014caf4cac115973b70
root <root@amol.com.amol.com>
2023-11-14 16:46:50 +0530

    Routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,8 +13,21 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+
+
+Route::post('register', 'App\Http\Controllers\AuthController@register');
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('books', BooksController::class);
+    Route::apiResource('categories', CategoryController::class);
+    Route::get('refresh', 'App\Http\Controllers\AuthController@refresh');
+});
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('books', [BooksController::class, 'index']);
+Route::get('books/{book}/category', [BooksController::class, 'category']);
